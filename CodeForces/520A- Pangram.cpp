28ed36aed31699f7c36345e7e0c1c724ae9cf725
Gabriel160204 <89217994+Gabriel160204@users.noreply.github.com>
2022-08-08 23:07:11 -0300

    a

--- CodeForces/520A- Pangram.cpp
+++ CodeForces/520A- Pangram.cpp
@@ -1,31 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-    int n; cin >> n;
-    string str; cin >> str;
+int main() {
+    int n, ans; 
+    string str; 
+    cin >> n >> str;
 
-    if(str.size() < 26)
-    {
-        cout << "NO\n";
+    for (char& c : str)
+        tolower(c);
+
+    sort(str.begin(), str.end());
+
+    for (int i = 0; i < n - 1; i++) {
+       if (str[i] == str[i + 1])
+            ans++;
     }
-    else
-    {
-        for_each(str.begin(), str.end(), [] (char &x) {x = ::tolower(x);});
-        sort(str.begin(), str.end());
-        for (int i = 0; i < str.size(); i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if (str[i] == str[j])
-                {
-                    str[i] = '\0';
-                }
-            }
-        }
-        str.erase(remove(str.begin(), str.end(), '\0'), str.end());
-        str.size() == 26 ? cout << "YES\n" : cout << "NO\n";
-    }
+    n - ans == 26 ? cout << "YES" : cout << "NO";
     return 0;
 }
