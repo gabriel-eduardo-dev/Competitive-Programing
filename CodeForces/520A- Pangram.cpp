f8d029df485cd352436b8f280900dbcc526b9723
Gabriel <89217994+Gabriel160204@users.noreply.github.com>
2022-11-01 14:57:57 -0300

    Update 520A- Pangram.cpp

--- CodeForces/520A- Pangram.cpp
+++ CodeForces/520A- Pangram.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define nn '\n'
+#define ll long long
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define io ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-
+ 
+// https://codeforces.com/contest/520/problem/A
+ 
 int main() {
-    int n, ans; 
-    string str; 
-    cin >> n >> str;
-
-    for (char& c : str)
-        tolower(c);
-
-    sort(str.begin(), str.end());
-
-    for (int i = 0; i < n - 1; i++) {
-       if (str[i] == str[i + 1])
-            ans++;
-    }
-    n - ans == 26 ? cout << "YES" : cout << "NO";
-    return 0;
+	io;
+ 
+	int size;
+	string str;
+	set<char> ss;
+	cin >> size >> str;
+ 
+	if (size < 26) {
+		cout << "NO\n";
+	}
+	else {
+		for (int i = 0; i < size; i++) {
+			ss.insert(tolower(str[i]));
+		}
+		(ss.size() == 26) ? cout << "YES\n"
+						  : cout << "NO\n";
+	}
 }
