d56801609f350a63d4ae58b7541b3886f9c5608a
gabriel-eduardo-dev <gabrie.eduardo.contact@gmail.com>
2023-07-24 17:06:04 -0300

    update template file

--- CodeForces/template.cpp
+++ CodeForces/template.cpp
@@ -2,37 +2,62 @@
 
 using namespace std;
 
-#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define rng_init	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
 #define rng_seed(x) mt19937 rng(x)
-#define all(cont) (cont).begin(), (cont).end()
-#define rall(cont) cont.end(), cont.begin()
-#define sz(x) (int)(x).size()
-#define PI 3.1415926535897932384626433832795
-#define mp make_pair
-#define eb emplace_back
-#define dbg1(v1)        cerr<<v1<<'\n'
-#define dbg2(v1,v2)     cerr<<v1<<'\t'<<v2<<'\n'
-#define dbg3(v1,v2,v3)  cerr<<v1<<'\t'<<v2<<'\t'<<v3<<'\n'
+#define all(cont)	(cont).begin(), (cont).end()
+#define rall(cont)	cont.end(), cont.begin()
+#define sz(x)		(int)(x).size()
+#define PI			3.1415926535897932384626433832795
+#define mp			make_pair
+#define eb			emplace_back
+template <typename T>
+void print_arg(const T &arg) {
+	cout << arg << ' ';
+}
+template <typename T, typename... Args>
+void print_arg(const T &arg, const Args &...args) {
+	cout << arg << ' ';
+	print_arg(args...);
+}
+template <typename... Args>
+void print(const Args &...args) {
+	print_arg(args...);
+	cout << '\n';
+}
+template <typename T>
+void read_arg(T &arg) {
+	cin >> arg;
+}
+template <typename T, typename... Args>
+void read_arg(T &arg, Args &...args) {
+	cin >> arg;
+	read_arg(args...);
+}
+template <typename... Args>
+void read(Args &...args) {
+	read_arg(args...);
+}
 
-using vi  = vector<int>;
+template <typename T>
+using vt = vector<T>;
+
+using vi = vector<int>;
 using vii = vector<vi>;
-using vs  = vector<string>;
-using ll  = long long;
+using vs = vector<string>;
+using ll = long long;
 using llu = long long unsigned int;
 
-void solve() {
-
-}
+void solve() {}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-    int TC = 1;
-	//cin >> TC;
+	int TC = 1;
+	// cin >> TC;
 	while (TC--) {
 		solve();
 	}
 
-  return 0;
+	return 0;
 }
